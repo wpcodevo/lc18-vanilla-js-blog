9a3f0b2583f4b2717bc63d0040c7cc4b51fc350e
Edem <ziddah.edem@gmail.com>
2021-07-25 21:01:53 +0000

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = {
           <a class="logo" href="#">CodingMega</a>
           <!-- menu -->
           <ul class="nav-list d-flex">
-            <li><a href="#">Home</a></li>
+            <li><a href="/#/">Home</a></li>
             <li><a href="/#/about">About</a></li>
             <li><a href="/#/contact">Contact Us</a></li>
           </ul>
